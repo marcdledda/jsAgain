3b94b2dde1f7c3c48c1cfbdfb2cd685f17d97f8b
marcdledda <marcdl636@gmail.com>
2018-01-25 10:47:55 -0600

    js experiments

---
+++ js/data.js
@@ -0,0 +1,12 @@
+let inventory = [
+    {
+        face: "A",
+        symbol: "Spade",
+        color: "Black"
+    },
+    {
+        face: "5",
+        symbol: "Diamond",
+        color: "Red"
+    }
+];
--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 console.log("JS TEST");
+
+let leData = document.getElementById("leData");
+
+// inventory.forEach(function(item, index){
+//     console.log("items", item, index);
+//     leData.innerHTML += `<li class="cards">${item.face}: ${item.symbol}</li>`
+// });
+
+inventory.forEach( (item, index) => {
+    console.log("items", item, index);
+    leData.innerHTML += `<li class="cards">${item.face}: ${item.symbol}</li>`;
+});
+
+let getCards = document.getElementsByClassName("cards");
+console.log("cards", getCards);
+console.log("card item", getCards[0].innerHTML);
+
+const howmanyCards = () => {
+    return inventory.length;
+};
